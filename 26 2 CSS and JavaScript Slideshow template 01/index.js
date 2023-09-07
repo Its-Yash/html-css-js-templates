05b6e081c0f8a572789990d8a7798aadc51c3bf8
Yash Pathak <90565364+Its-Yash@users.noreply.github.com>
2023-09-07 16:17:50 +0530

    Add files via upload

---
+++ 26 2 CSS and JavaScript Slideshow template 01/index.js
@@ -0,0 +1,33 @@
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const images = document.querySelectorAll(".card");
+const totalImages = images.length;
+let index = 0;
+
+prev.addEventListener("click", () => {
+  nextImage("prev");
+});
+
+next.addEventListener("click", () => {
+  nextImage("next");
+});
+
+function nextImage(direction) {
+  if (direction === "next") {
+    index++;
+    if (index === totalImages) {
+      index = 0;
+    }
+  } else if (direction === "prev") {
+    if (index == 0) {
+      index = totalImages - 1;
+    } else {
+      index--;
+    }
+  }
+
+  for (let i = 0; i < images.length; i++) {
+    images[i].classList.add("hidden-card");
+  }
+  images[index].classList.remove("hidden-card");
+}
